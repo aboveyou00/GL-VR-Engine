9f68c251af7528a35376a42cd0ce7a45b010ccf5
aboveyou00 <brandonyoyoslade@gmail.com>
2016-06-29 13:53:13 -0600

    More work on GraphicsObjectImpl to get something displaying

--- GlEngine/GraphicsObjectImpl.cpp
+++ GlEngine/GraphicsObjectImpl.cpp
@@ -20,15 +20,15 @@ namespace GlEngine
         {
             if (!arrayVbo)
             {
-                VboFactory<VboType::Float, Vector<3>, Vector<4>> factory(BufferMode::Array);
+                VboFactory<VboType::Float, Vector<3>, Vector<3>> factory(BufferMode::Array);
 
-                factory.AddVertex({ 0, 0, 0 }, { 1.f, 0.f, 0.5f, 1.f });
-                factory.AddVertex({ .5f, 0, 0 }, { 1.f, 0.f, 0.5f, 1.f });
-                factory.AddVertex({ 0, .5f, 0 }, { 1.f, 0.f, 0.5f, 1.f });
+                factory.AddVertex({ 1.f, 0.f, 0.5f }, { 0, 0, 0 });
+                factory.AddVertex({ 1.f, 0.f, 0.5f }, { .5f, 0, 0 });
+                factory.AddVertex({ 1.f, 0.f, 0.5f }, { 0, .5f, 0 });
 
-                factory.AddVertex({ 0, 0, 0 }, { 0.5f, 0.f, 1.f, 1.f });
-                factory.AddVertex({ .2f, .5f, 0 }, { 0.5f, 0.f, 1.f, 1.f });
-                factory.AddVertex({ .5f, .2f, 0 }, { 0.5f, 0.f, 1.f, 1.f });
+                factory.AddVertex({ 0.5f, 0.f, 1.f }, { 0, 0, 0 });
+                factory.AddVertex({ 0.5f, 0.f, 1.f }, { .2f, .5f, 0 });
+                factory.AddVertex({ 0.5f, 0.f, 1.f }, { .5f, .2f, 0 });
 
                 arrayVbo = factory.Compile();
                 if (!arrayVbo.Initialize()) return false;
@@ -60,11 +60,29 @@ namespace GlEngine
 		{
             if (arrayVbo && elementVbo)
             {
-                arrayVbo.MakeCurrent();
-                elementVbo.MakeCurrent();
+                glPushClientAttrib(GL_CLIENT_VERTEX_ARRAY_BIT);
+                glEnableClientState(GL_VERTEX_ARRAY);
 
-                //glDrawArrays(GL_TRIANGLES, 0, 2);
-                glDrawElements(GL_TRIANGLES, 2, static_cast<GLenum>(VboType::UnsignedInt), nullptr);
+                arrayVbo.MakeCurrent();
+                glInterleavedArrays(GL_C3F_V3F, sizeof(float) * 6, nullptr);
+
+                //glDrawArrays(GL_TRIANGLES, 0, 18);
+                elementVbo.MakeCurrent();
+                glDrawElements(GL_TRIANGLES, 18, static_cast<GLenum>(VboType::UnsignedInt), nullptr);
+
+                glPopClientAttrib();
+
+
+
+                //glBegin(GL_TRIANGLES);
+                //
+                //glColor3d(0.0, 1.0, 0.0);
+                //
+                //glVertex3d(0.4, 0, 0.5);
+                //glVertex3d(0.4, 0.4, 0.5);
+                //glVertex3d(0, 0.4, 0.5);
+                //
+                //glEnd();
             }
 		}
 	}
