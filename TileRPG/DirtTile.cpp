35edf58edd4cab3075c769aae1a99a643cd3dcae
aboveyou00 <brandonyoyoslade@gmail.com>
2016-08-05 13:25:25 -0600

    Randomly select between dirt and rock tiles

--- TileRPG/DirtTile.cpp
+++ TileRPG/DirtTile.cpp
@@ -2,6 +2,10 @@
 #include "DirtTile.h"
 #include "TileManager.h"
 
+#include "ChunkGraphicsObject.h"
+#include "Shader.h"
+#include "Texture.h"
+
 namespace TileRPG
 {
     DirtTile::DirtTile()
@@ -13,4 +17,12 @@ namespace TileRPG
     }
 
     SINGLETON_TILE_CPP(DirtTile);
+
+    void DirtTile::AddToChunkGraphicsObject(ChunkGraphicsObject &chunkGobj, int x, int y, int z)
+    {
+        static auto shader = GlEngine::Shader::Create("Shaders", "direct_light_tex");
+        static auto texture = GlEngine::Texture::FromFile("Textures/blue.png");
+        chunkGobj.SetGraphics(shader, texture);
+        ITile::AddToChunkGraphicsObject(chunkGobj, x, y, z);
+    }
 }
--- TileRPG/IChunkProvider.cpp
+++ TileRPG/IChunkProvider.cpp
@@ -7,6 +7,7 @@
 #include "TileManager.h"
 
 #include "DirtTile.h"
+#include "RockTile.h"
 #include "AirTile.h"
 
 namespace TileRPG
@@ -22,21 +23,15 @@ namespace TileRPG
     {
         ITile *air = &AirTile::GetInstance();
         ITile *dirt = &DirtTile::GetInstance();
+        ITile *rock = &RockTile::GetInstance();
 
         auto chunk = new Chunk(x, z);
         for (size_t q = 0; q < Chunk::TILES_PER_CHUNK_X; q++)
             for (size_t w = 0; w < Chunk::TILES_PER_CHUNK_Z; w++)
             {
                 chunk->SetTileInfo(q, 0, w, dirt);
-                chunk->SetTileInfo(q, 1, w, (rand() % 10 < 9) ? dirt : air);
-                chunk->SetTileInfo(q, 2, w, (rand() % 10 < 8) ? dirt : air);
-                chunk->SetTileInfo(q, 3, w, (rand() % 10 < 7) ? dirt : air);
-                chunk->SetTileInfo(q, 4, w, (rand() % 10 < 6) ? dirt : air);
-                chunk->SetTileInfo(q, 5, w, (rand() % 10 < 5) ? dirt : air);
-                chunk->SetTileInfo(q, 6, w, (rand() % 10 < 4) ? dirt : air);
-                chunk->SetTileInfo(q, 7, w, (rand() % 10 < 3) ? dirt : air);
-                chunk->SetTileInfo(q, 8, w, (rand() % 10 < 2) ? dirt : air);
-                chunk->SetTileInfo(q, 9, w, (rand() % 10 < 1) ? dirt : air);
+                chunk->SetTileInfo(q, 1, w, (rand() % 10 < 5) ? dirt : rock);
+                chunk->SetTileInfo(q, 2, w, (rand() % 10 < 2) ? rock : air);
             }
 
         return chunk;
