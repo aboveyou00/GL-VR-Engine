c2b3856374c67a2642857b12a459d9e0d1c07aa8
aboveyou00 <brandonyoyoslade@gmail.com>
2016-08-08 12:36:02 -0600

    Improve shader output formatting

--- GlEngine/StringUtils.h
+++ GlEngine/StringUtils.h
@@ -15,6 +15,9 @@ namespace GlEngine
 		ENGINE_SHARED bool getf(std::istream& in, float &value) noexcept;
         ENGINE_SHARED int strcmp(const char *const lhs, const char *const rhs) noexcept;
 
+        ENGINE_SHARED bool is_empty(const char *const str) noexcept;
+        ENGINE_SHARED bool is_empty_or_ws(const char *const str) noexcept;
+
         //IMPORTANT: The return value of this method is invalidated when you call it again, unless it doesn't find another token
         //This method makes the assumption that pos is a valid index in this string; str + pos should never be after the terminating \0 on str
         template <size_t buffSize = 32>
