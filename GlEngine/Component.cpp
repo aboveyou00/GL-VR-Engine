f0e3f76aa146bc69afbcbd53509ed783e2fc8593
aboveyou00 <brandonyoyoslade@gmail.com>
2017-01-09 12:15:58 -0700

    fix(shader-factory): temporarily remove line numbering in shader source

--- GlEngine/Component.cpp
+++ GlEngine/Component.cpp
@@ -106,11 +106,10 @@ namespace GlEngine
             stream << "}" << std::endl;
 
             compiled = stream.str();
-            int linen = 1;
-            std::function<std::string(std::smatch)> linen_cb = [&](std::smatch match) -> std::string {
-                return "\n/* " + Util::itos(++linen) + " */ ";
-            };
-            compiled = "/* 1 */ " + Util::regex_replace_callback(compiled, R"raw(\r?\n)raw"s, linen_cb);
+            //int linen = 1;
+            //compiled = "/* 1 */ " + Util::regex_replace_callback(compiled, R"raw(\r?\n)raw"s, [&](std::smatch match) -> std::string {
+            //    return "\n/* " + Util::itos(++linen) + " */ ";
+            //});
             return compiled;
         }
 
