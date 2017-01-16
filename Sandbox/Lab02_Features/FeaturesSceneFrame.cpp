bc826392be73e192c5ef06ae34a5010dd082e05b
aboveyou00 <brandonyoyoslade@gmail.com>
2017-01-16 14:59:02 -0700

    fix(obj-loader): allow more than one obj to be loaded at a time

--- Sandbox/Lab02_Features/FeaturesSceneFrame.cpp
+++ Sandbox/Lab02_Features/FeaturesSceneFrame.cpp
@@ -22,21 +22,21 @@ bool FeaturesSceneFrame::Initialize()
     cameraObject->SetLock(GlEngine::CameraLock::RELATIVE_POSITION);
     cameraObject->SetPosition({ 0, -3.5, 7 });
 
-    //auto lightSource1 = CreateGameObject<LightSourceObject>();
-    //auto torus1 = CreateGameObject<PhongTorus>(Vector<3> { 1, .15f, .3f }, Vector<3> { .4f, .6f, .6f }, lightSource1->lightSource(), Vector<3> { 0, 0, 1 });
-    //torus1->SetPosition({ 0, 0, 0 });
+    auto lightSource1 = CreateGameObject<LightSourceObject>();
+    auto torus1 = CreateGameObject<PhongTorus>(Vector<3> { 1, .15f, .3f }, Vector<3> { .4f, .6f, .6f }, lightSource1->lightSource(), Vector<3> { 0, 0, 1 });
+    torus1->SetPosition({ 0, 0, 0 });
 
-    //auto lightSource2 = CreateGameObject<LightSourceObject>();
-    //auto torus2 = CreateGameObject<PhongTorus>(Vector<3> { .2f, .6f, .75f }, Vector<3> { .05f, .05f, .05f }, lightSource2->lightSource(), Vector<3> { 1, 0, 0 });
-    //torus2->SetPosition({ -5, 0, -5 });
+    auto lightSource2 = CreateGameObject<LightSourceObject>();
+    auto torus2 = CreateGameObject<PhongTorus>(Vector<3> { .2f, .6f, .75f }, Vector<3> { .05f, .05f, .05f }, lightSource2->lightSource(), Vector<3> { 1, 0, 0 });
+    torus2->SetPosition({ -5, 0, -5 });
 
-    //auto lightSource3 = CreateGameObject<LightSourceObject>();
-    //auto torus3 = CreateGameObject<PhongTorus>(Vector<3> { .4f, .4f, .4f }, Vector<3> { .9f, .9f, .9f }, lightSource3->lightSource(), Vector<3> { 0, 1, 0 });
-    //torus3->SetPosition({ 5, 0, -5 });
+    auto lightSource3 = CreateGameObject<LightSourceObject>();
+    auto torus3 = CreateGameObject<PhongTorus>(Vector<3> { .4f, .4f, .4f }, Vector<3> { .9f, .9f, .9f }, lightSource3->lightSource(), Vector<3> { 0, 1, 0 });
+    torus3->SetPosition({ 5, 0, -5 });
 
     auto lightSource4 = CreateGameObject<LightSourceObject>();
     auto teapot = CreateGameObject<NoLidTeapot>(Vector<3> { .4f, .4f, .4f }, Vector<3> { .9f, .9f, .9f }, lightSource4->lightSource(), Vector<3> { 0, 1, 0 });
-    teapot->SetPosition({ 0, 0, 0 });
+    teapot->SetPosition({ 7, 0, 2 });
 
     return true;
 }
