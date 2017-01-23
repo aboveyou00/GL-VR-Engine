775b84fa2b5f2cf075876b28a0c772fb3ee52c7c
aboveyou00 <brandonyoyoslade@gmail.com>
2017-01-23 11:14:37 -0700

    feat(sandbox): add keyboard controls to lab3

---
+++ Sandbox/Lab03_LightsAndEffects/Lab3Controls.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../LabControls.h"
+
+class Lab3Controls : public LabControls
+{
+public:
+    Lab3Controls();
+    ~Lab3Controls();
+
+    static unsigned celShadingSteps;
+    static float spotlightCutoffAngle;
+    static float spotlightAttenuation;
+
+    virtual void HandleEvent(Event &evt);
+
+    virtual const char *name() override;
+
+private:
+    static const unsigned ATTENTUATION_VALUE_COUNT = 4;
+    static float attenuationValues[ATTENTUATION_VALUE_COUNT];
+    unsigned attenuationIdx;
+};
