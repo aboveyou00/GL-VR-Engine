150736e3a36d2305a75705351450a2aa328b42ad
aboveyou00 <brandonyoyoslade@gmail.com>
2017-01-23 12:20:12 -0700

    feat(sandbox): add Tree game object and create 25 of them

---
+++ Sandbox/Lab03_LightsAndEffects/Tree.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "GameObject.h"
+
+namespace GlEngine::ShaderFactory
+{
+    class IPropertyProvider;
+}
+typedef GlEngine::ShaderFactory::IPropertyProvider IPropertyProvider;
+
+class Tree : public GlEngine::GameObject
+{
+public:
+    Tree(Vector<3> position, std::vector<IPropertyProvider*> providers);
+    ~Tree();
+
+    virtual const char *name() override;
+
+    virtual GlEngine::GraphicsObject *CreateGraphicsObject(GlEngine::GraphicsContext &ctx) override;
+
+private:
+    std::vector<IPropertyProvider*> providers;
+};
