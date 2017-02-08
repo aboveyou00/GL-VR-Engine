3477e82a58a79d230f3bb06e1030ee37a260cdff
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-08 15:41:34 -0700

    feat(sandbox): make RawGraphicsObject compose an ObjGraphicsObject rather than extending one

--- Sandbox/RawGraphicsObject.cpp
+++ Sandbox/RawGraphicsObject.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "RawGraphicsObject.h"
+#include "ObjGraphicsObject.h"
 
 #include "VboGraphicsSection.h"
 #include "RawShaderFactory.h"
@@ -7,11 +8,91 @@
 
 typedef GlEngine::ShaderFactory::RawShaderFactory RawShaderFactory;
 
-RawGraphicsObject::RawGraphicsObject(const char * const filename, GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties)
-    : ObjGraphicsObject(filename, [shaderSource, properties](GlEngine::Material*) { return new RawShaderFactory(shaderSource, properties); })
+RawGraphicsObject::RawGraphicsObject(GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties)
+    : GlEngine::GraphicsObject(false)
 {
+    createFactory = [shaderSource, properties](GlEngine::Material*) { return new RawShaderFactory(shaderSource, properties); };
 }
-
+RawGraphicsObject::RawGraphicsObject(const char *const filename, GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties)
+    : RawGraphicsObject(shaderSource, properties)
+{
+    _compositeObj = new GlEngine::ObjGraphicsObject(filename, createFactory);
+}
 RawGraphicsObject::~RawGraphicsObject()
 {
+    SafeDelete(_compositeObj);
+}
+
+bool RawGraphicsObject::Initialize()
+{
+    return _compositeObj->Initialize();
+}
+void RawGraphicsObject::Shutdown()
+{
+    _compositeObj->Shutdown();
+}
+bool RawGraphicsObject::InitializeGraphics()
+{
+    return _compositeObj->InitializeGraphics();
+}
+void RawGraphicsObject::ShutdownGraphics()
+{
+    _compositeObj->ShutdownGraphics();
+}
+
+void RawGraphicsObject::SetMaterial(GlEngine::Material *material)
+{
+    auto vbobj = dynamic_cast<GlEngine::VboGraphicsObject*>(_compositeObj);
+    assert(vbobj != nullptr);
+    vbobj->SetMaterial(material);
+}
+
+void RawGraphicsObject::AddPropertyProvider(IPropertyProvider *provider)
+{
+    _compositeObj->AddPropertyProvider(provider);
+}
+void RawGraphicsObject::RemovePropertyProvider(IPropertyProvider *provider)
+{
+    _compositeObj->RemovePropertyProvider(provider);
+}
+
+void RawGraphicsObject::BuildVao(GlEngine::VaoFactory &vao)
+{
+    _compositeObj->BuildVao(vao);
+}
+
+void RawGraphicsObject::PreRender(GlEngine::RenderTargetLayer layer)
+{
+    _compositeObj->PreRender(layer);
+}
+void RawGraphicsObject::RenderImpl(GlEngine::RenderTargetLayer layer)
+{
+    _compositeObj->RenderImpl(layer);
+}
+void RawGraphicsObject::PostRender(GlEngine::RenderTargetLayer layer)
+{
+    _compositeObj->PostRender(layer);
+}
+
+void RawGraphicsObject::PreRenderInstanced(GlEngine::RenderTargetLayer layer)
+{
+    _compositeObj->PreRenderInstanced(layer);
+}
+void RawGraphicsObject::RenderInstancedImpl(GlEngine::RenderTargetLayer layer, unsigned instanceCount)
+{
+    _compositeObj->RenderInstancedImpl(layer, instanceCount);
+}
+void RawGraphicsObject::PostRenderInstanced(GlEngine::RenderTargetLayer layer)
+{
+    _compositeObj->PostRenderInstanced(layer);
+}
+
+std::string RawGraphicsObject::name()
+{
+    return "RawGraphicsObject";
+}
+
+RawGraphicsObject::operator bool()
+{
+    return _compositeObj && !!*_compositeObj;
 }
--- Sandbox/RawGraphicsObject.h
+++ Sandbox/RawGraphicsObject.h
@@ -1,13 +1,65 @@
 #pragma once
 
-#include "ObjGraphicsObject.h"
+#include "GraphicsObject.h"
 #include "ShaderFactory.h"
+#include "RenderTargetLayer.h"
+#include "VBOFactory.h"
+#include "VboFactoryGraphicsObject.h"
+
+namespace GlEngine
+{
+    class ObjGraphicsObject;
+    class Material;
+
+    namespace ShaderFactory
+    {
+        class IPropertyProvider;
+    }
+}
 
 typedef GlEngine::ShaderFactory::ShaderFactory ShaderFactory;
+typedef GlEngine::ShaderFactory::IPropertyProvider IPropertyProvider;
 
-class RawGraphicsObject : public GlEngine::ObjGraphicsObject
+class RawGraphicsObject : public GlEngine::GraphicsObject
 {
+private:
+    RawGraphicsObject(GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties);
+
 public:
-    RawGraphicsObject(const char * const filename, GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties);
+    template <GlEngine::VboType type, typename... TArgs>
+    RawGraphicsObject(GlEngine::VboFactory<type, TArgs...> *factory, GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties)
+        : RawGraphicsObject(shaderSource, properties)
+    {
+        _compositeObj = new GlEngine::VboFactoryGraphicsObject<type, TArgs...>(factory);
+    }
+    RawGraphicsObject(const char *const filename, GlEngine::ShaderFactory::ShaderSource* shaderSource, std::map<size_t, GlEngine::ShaderFactory::ShaderProp*>* properties);
     ~RawGraphicsObject();
+
+    virtual bool Initialize() override;
+    virtual void Shutdown() override;
+    virtual bool InitializeGraphics() override;
+    virtual void ShutdownGraphics() override;
+
+    void SetMaterial(GlEngine::Material *material);
+
+    virtual void AddPropertyProvider(IPropertyProvider *provider) override;
+    virtual void RemovePropertyProvider(IPropertyProvider *provider) override;
+
+    virtual void BuildVao(GlEngine::VaoFactory &vao) override;
+
+    virtual void PreRender(GlEngine::RenderTargetLayer layer) override;
+    virtual void RenderImpl(GlEngine::RenderTargetLayer layer) override;
+    virtual void PostRender(GlEngine::RenderTargetLayer layer) override;
+
+    virtual void PreRenderInstanced(GlEngine::RenderTargetLayer layer) override;
+    virtual void RenderInstancedImpl(GlEngine::RenderTargetLayer layer, unsigned instanceCount) override;
+    virtual void PostRenderInstanced(GlEngine::RenderTargetLayer layer) override;
+
+    virtual std::string name() override;
+
+    virtual operator bool() override;
+
+private:
+    GlEngine::GraphicsObject *_compositeObj;
+    std::function<GlEngine::ShaderFactory::ShaderFactory*(GlEngine::Material*)> createFactory;
 };
