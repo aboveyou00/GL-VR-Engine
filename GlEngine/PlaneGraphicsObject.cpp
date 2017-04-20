78e6e3e600a24610113fb5c8d24d5c4589b3708f
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-20 10:49:10 -0600

    feat(engine): allow PlaneGraphicsObject to face in any of six directions

--- GlEngine/PlaneGraphicsObject.cpp
+++ GlEngine/PlaneGraphicsObject.cpp
@@ -3,8 +3,8 @@
 
 namespace GlEngine
 {
-    PlaneGraphicsObject::PlaneGraphicsObject(std::string name, Material *mat, Vector<2> size, Vector<2> subdivisions, Vector<2> uvScale)
-        : VboGraphicsObject(name), _mat(mat), _size(size), _subdivisions(subdivisions), _uvScale(uvScale)
+    PlaneGraphicsObject::PlaneGraphicsObject(std::string name, Material *mat, Vector<2> size, Vector<2> subdivisions, Vector<2> uvScale, PlaneDirection dir)
+        : VboGraphicsObject(name), _mat(mat), _size(size), _subdivisions(subdivisions), _uvScale(uvScale), _dir(dir)
     {
     }
     PlaneGraphicsObject::~PlaneGraphicsObject()
@@ -19,13 +19,14 @@ namespace GlEngine
 
         const float x = -_size[0] / 2, y = 0, z = -_size[1] / 2;
         const float w = _size[0], h = 0, d = _size[1];
-        int idx0, idx1, idx2, idx3;
+        int idx0 = 0, idx1 = 0, idx2 = 0, idx3 = 0;
 
         //Render face Yp
         auto ww = w / _subdivisions[0];
         auto dd = d / _subdivisions[1];
         auto uvw = _uvScale[0] / _subdivisions[0];
         auto uvd = _uvScale[1] / _subdivisions[1];
+
         for (size_t q = 0; q < _subdivisions[0]; q++)
         {
             auto tx = x + q * ww;
@@ -35,11 +36,32 @@ namespace GlEngine
             {
                 auto tz = z + e * dd;
                 auto uvz = e * uvd;
-
-                idx0 = AddVertex({ tx,      y + h, tz      }, { uvx,       uvz       }, { 0, -1, 0 });
-                idx1 = AddVertex({ tx + ww, y + h, tz      }, { uvx + uvw, uvz       }, { 0, -1, 0 });
-                idx2 = AddVertex({ tx + ww, y + h, tz + dd }, { uvx + uvw, uvz + uvd }, { 0, -1, 0 });
-                idx3 = AddVertex({ tx,      y + h, tz + dd }, { uvx,       uvz + uvd }, { 0, -1, 0 });
+                
+                if (_dir == PlaneDirection::Xp || _dir == PlaneDirection::Xm)
+                {
+                    auto normal = _dir == PlaneDirection::Xp ? -1 : 1;
+                    idx0 = AddVertex({ y + h, tx,      tz      }, { uvx,       uvz       }, { normal, 0, 0 });
+                    idx1 = AddVertex({ y + h, tx + ww, tz      }, { uvx + uvw, uvz       }, { normal, 0, 0 });
+                    idx2 = AddVertex({ y + h, tx + ww, tz + dd }, { uvx + uvw, uvz + uvd }, { normal, 0, 0 });
+                    idx3 = AddVertex({ y + h, tx,      tz + dd }, { uvx,       uvz + uvd }, { normal, 0, 0 });
+                }
+                else if (_dir == PlaneDirection::Yp || _dir == PlaneDirection::Ym)
+                {
+                    auto normal = _dir == PlaneDirection::Yp ? -1 : 1;
+                    idx0 = AddVertex({ tx,      y + h, tz      }, { uvx,       uvz       }, { 0, normal, 0 });
+                    idx1 = AddVertex({ tx + ww, y + h, tz      }, { uvx + uvw, uvz       }, { 0, normal, 0 });
+                    idx2 = AddVertex({ tx + ww, y + h, tz + dd }, { uvx + uvw, uvz + uvd }, { 0, normal, 0 });
+                    idx3 = AddVertex({ tx,      y + h, tz + dd }, { uvx,       uvz + uvd }, { 0, normal, 0 });
+                }
+                else if (_dir == PlaneDirection::Zp || _dir == PlaneDirection::Zm)
+                {
+                    auto normal = _dir == PlaneDirection::Zp ? -1 : 1;
+                    idx0 = AddVertex({ tx,      tz,      y + h }, { uvx,       uvz       }, { 0, 0, normal });
+                    idx1 = AddVertex({ tx + ww, tz,      y + h }, { uvx + uvw, uvz       }, { 0, 0, normal });
+                    idx2 = AddVertex({ tx + ww, tz + dd, y + h }, { uvx + uvw, uvz + uvd }, { 0, 0, normal });
+                    idx3 = AddVertex({ tx,      tz + dd, y + h }, { uvx,       uvz + uvd }, { 0, 0, normal });
+                }
+                else assert(false);
 
                 AddTriangle({ idx0, idx1, idx2 });
                 AddTriangle({ idx0, idx2, idx3 });
--- GlEngine/PlaneGraphicsObject.h
+++ GlEngine/PlaneGraphicsObject.h
@@ -5,25 +5,20 @@
 
 namespace GlEngine
 {
+    enum class PlaneDirection: uint32_t
+    {
+        Xp = 0,
+        Yp,
+        Zp,
+        Xm,
+        Ym,
+        Zm
+    };
+
     class ENGINE_SHARED PlaneGraphicsObject : public VboGraphicsObject
     {
     public:
-        PlaneGraphicsObject(std::string name, Material *mat, Vector<2> size = { 1, 1 }, Vector<2> subdivisions = { 1, 1 }, Vector<2> uvScale = { 1, 1 });
-        template <typename... TArgs>
-        PlaneGraphicsObject(std::string name, Vector<2> size, TArgs... args)
-            : PlaneGraphicsObject(name, new PhongMaterial(args...), size)
-        {
-        }
-        template <typename... TArgs>
-        PlaneGraphicsObject(std::string name, Vector<2> size, Vector<2> subdivisions, TArgs... args)
-            : PlaneGraphicsObject(name, new PhongMaterial(args...), size, subdivisions)
-        {
-        }
-        template <typename... TArgs>
-        PlaneGraphicsObject(std::string name, Vector<2> size, Vector<2> subdivisions, Vector<2> uvScale, TArgs... args)
-            : PlaneGraphicsObject(name, new PhongMaterial(args...), size, subdivisions, uvScale)
-        {
-        }
+        PlaneGraphicsObject(std::string name, Material *mat, Vector<2> size = { 1, 1 }, Vector<2> subdivisions = { 1, 1 }, Vector<2> uvScale = { 1, 1 }, PlaneDirection dir = PlaneDirection::Yp);
         ~PlaneGraphicsObject();
 
         virtual bool InitializeAsync() override;
@@ -31,5 +26,6 @@ namespace GlEngine
     private:
         Material *_mat;
         Vector<2> _size, _subdivisions, _uvScale;
+        PlaneDirection _dir;
     };
 }
