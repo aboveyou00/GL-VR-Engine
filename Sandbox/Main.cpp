6e072526d3089bdf083d0f3f14ba8d4a50afdc24
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-20 13:21:45 -0600

    refactor(sandbox): remove Sandbox and SandboxLoop; use Engine::QuickLaunch instead

--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -1,22 +1,23 @@
 #include "stdafx.h"
-#include "Sandbox.h"
+#include "Engine.h"
 
-#include "Program.h"
-#include "Attribute.h"
-#include "VboPropertySource.h"
-#include "UniformPropertySource.h"
-
-using namespace GlEngine::ShaderFactory;
+#include "Lab01_Diffuse/DiffuseSceneFrame.h"
+#include "Lab02_Features/FeaturesSceneFrame.h"
+#include "Lab03_LightsAndEffects/LightsAndEffectsSceneFrame.h"
+#include "Lab04_Textures/TexturesSceneFrame.h"
+#include "Lab05_Geometry/GeometrySceneFrame.h"
+#include "Lab06_Distortion/DistortionSceneFrame.h"
+#include "Lab07_Particles/ParticlesSceneFrame.h"
+#include "Lab08_Slenderman/SlendermanSceneFrame.h"
+#include "Lab08_Slenderman/SlendermanStaticSceneFrame.h"
+#include "Lab09_Spatpart/SpatpartSceneFrame.h"
 
 int main(int, char**)
 {
-    this_thread_name() = "main";
-    this_thread_type() = ThreadType::WndProc;
-
-    Sandbox sandbox;
-
-    if (!sandbox.Initialize()) return -1;
-    sandbox.MessageLoop();
-    sandbox.Shutdown();
-    return 0;
+    return THIS_ENGINE.QuickLaunchFrame<SpatpartSceneFrame>(
+        "Sandbox"s,     //Name of game
+        "sandbox.log"s, //Name of log file
+        "sandbox.cfg"s, //Name of config file
+        200u            //Target frame rate
+    );
 }
